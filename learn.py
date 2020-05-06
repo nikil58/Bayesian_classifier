b631c9c8358994b9da3cd94902342cf6c98a0c79
Nikita <titata1@mail.ru>
2020-05-06 23:13:58 +0300

    Added help button, redacted spam base

--- learn.py
+++ learn.py
@@ -15,7 +15,7 @@ def read(read_file):
     y = pd.get_dummies(df['number'])['spam']  # Шифрование надпими spam
     our = vectorizer.transform(test['text'])  # Шифруем текст с произвольного файла, не составляем словарь
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33,
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.26,
                                                         random_state=42)  # Разделяем выборку на тестовую и тенировочную
     clf = BernoulliNB()  # Создаем модель обучения
     clf.fit(x_train, y_train)  # Заполняем нашу модель тренировочными данными
@@ -26,15 +26,15 @@ def read(read_file):
 
     pred = clf.predict(our)  # Запускаем нашу модель на нашем произвольном файле
     result.insert(0, 'category', pred)  # Вставляем результат в колонку категорий
-    result.to_csv('test.csv', encoding='utf-8', sep=';', index=False)  # экспортируем в базу данных
+    result.to_csv(read_file, encoding='utf-8', sep=';', index=False)  # экспортируем в базу данных
 
-    res = pd.read_csv(r'test.csv', encoding='utf-8', delimiter=';')  # читаем получишийся файл
+    res = pd.read_csv(read_file, encoding='utf-8', delimiter=';')  # читаем получишийся файл
     for i in range(len(res.index)):
         if (res['category'][i] == 0):
             res['category'][i] = 'hash'  # меняем нули на hash
         else:
             res['category'][i] = 'spam'  # меняем 1 на spam
-    res.to_csv('test.csv', encoding='utf-8', sep=';', index=False)  # снова экспортируем в этот же файл
+    res.to_csv(read_file, encoding='utf-8', sep=';', index=False)  # снова экспортируем в этот же файл
 
 def text(s):
     df = pd.read_csv('train.csv', encoding='utf-8',
--- menu.py
+++ menu.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 from learn import read, text
 
 def open_file():
+    """Функция, отвечающая за открытие любого файла"""
     file_name = fd.askopenfilename()
     read(file_name)
     messagebox.showinfo("Done!","Check out your file")
@@ -12,9 +13,18 @@ def press_enter(event):
     s=input_text.get()
     messagebox.showinfo("Done!",text(s))
 
+def help_open():
+    help='''Чтобы начать работу, вам достаточно ввести ваше сообщение и нажать Enter.
+Появится окно, которое покажет вам спам это или нет.
+Также вы можете открыть любой файл в формате .cvs (Таблица exel с разделителем - ;), в которой у вас имеется 2 не пустых столбца.
+Желательный вид: первый столбец содержит любую инфурмацию (можно просто нули), а второй - ваши сообщения.
+После ваш файл отредактируется и первый столбец будет подписан, и покажет вам, что спам, а что нет'''
+    messagebox.showinfo("Help", help)
+
+
 root = Tk()
-root.title("фильтрация спама")                     #название окна
-root.geometry("800x600+300+100")                        #размеры окна и смещение его в центр
+root.title("Фильтрация спама")                     #название окна
+root.geometry("800x350+300+100")                        #размеры окна и смещение его в центр
 root.resizable(False, False) #Отменить возможнось расширения окна
 
 input_text=StringVar()
@@ -38,7 +48,9 @@ label_or=Label(text = "OR",   #подпись(что делать)
               font="Arial 16"
               )
 label_or.place (x=345, y=165)
-open_button=Button(text="Choose file", command = open_file)
-open_button.place(x=350, y=250)
+open_button=Button(text="Choose file", command = open_file, font=16)
+open_button.place(x=340, y=250, height=40, width=90)
 
+help_button=Button(text="Help", command = help_open, font=16)
+help_button.place(x=700, y=300, height=40, width=90)
 root.mainloop()
